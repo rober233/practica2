1f7a95d048beb4da70c79437f7d3f0609add0cfc
rober233 <98168060+rober233@users.noreply.github.com>
2022-01-22 14:04:47 +0100

    Update modificarhtml.py

--- modificarhtml.py
+++ modificarhtml.py
@@ -10,9 +10,9 @@ os.environ['GROUP_NUMBER']="18"
 num_grupo= str(os.environ.get('GROUP_NUMBER'))
 
 #Modificamos el titulo
-call(['cp '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage.html '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage1.html'], shell=True)
-copia = open( cwd + '/practica_creativa2/bookinfo/src/productpage/templates/productpage1.html', 'r')
-f = open( cwd + '/practica_creativa2/bookinfo/src/productpage/templates/productpage.html', 'w')
+call(['cp '+cwd+'/templates/productpage.html '+cwd+'/templates/productpage1.html'], shell=True)
+copia = open( cwd + '/templates/productpage1.html', 'r')
+f = open( cwd + '/templates/productpage.html', 'w')
 for line in copia:
     if "{% block title %}Simple Bookstore App{% endblock %}" in line:
         f.write("{% block title %}" + num_grupo + "{% endblock %}")
@@ -20,6 +20,6 @@ for line in copia:
         f.write(line)
 f.close()
 copia.close()
-call(['rm '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage1.html'],shell=True)
+call(['rm '+cwd+'/templates/productpage1.html'],shell=True)
 #Lanzamos la aplicacion
-call(["sudo python3 practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 9080"], shell=True)
+call(["sudo python3 productpage_monolith.py 9080"], shell=True)
